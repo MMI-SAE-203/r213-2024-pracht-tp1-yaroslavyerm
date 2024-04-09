646549e4dbf3f67b932c480cf64234c0e5322dab
yaroslavyerm <yaroslav.yermakov@edu.univ-fcomte.fr>
2024-04-09 15:17:47 +0200

    ajouté la button de menu et les options

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,18 @@ onErrorCaptured((err, instance, info) => {
 
 <template>
   <header>
-    <nav>
+    <button
+      aria-controls="mainNav"
+      aria-expanded="true"
+      class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+      >
+      menu
+    </button>
+    <nav id="mainNav">
       <ul>
-        <li>
-          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-        </li>
+        <li><a href="#">item 1</a></li>
+        <li><a href="#">item 2</a></li>
+        <li><a href="#">item 3</a></li>
       </ul>
     </nav>
   </header>
