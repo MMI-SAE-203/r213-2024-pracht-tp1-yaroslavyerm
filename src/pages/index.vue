f52af11c08c89e3249754b1b11e819ccdd62fef4
yaroslavyerm <yaroslav.yermakov@edu.univ-fcomte.fr>
2024-04-09 15:46:35 +0200

    ajouté texte et boutons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,4 +4,16 @@
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
+  <section>
+    <button class="text-xl">bouton 1</button>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam nisi odio vitae consectetur, neque voluptate nam earum fugiat blanditiis, animi unde deserunt soluta tenetur, recusandae molestiae quasi sapiente pariatur nostrum.</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>Dolore officia pariatur, accusamus vero quibusdam consequuntur necessitatibus voluptatibus nobis fugit a eos omnis repellendus. Magnam, porro voluptates. Saepe adipisci est corporis commodi eaque nulla eveniet illo esse recusandae animi!</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>Sed, iusto aspernatur ipsa incidunt, minima fugit necessitatibus sapiente quo iure delectus doloremque officia voluptatibus ipsam inventore. Molestias eius molestiae quia praesentium placeat sequi ut repellendus velit, accusantium facere voluptate?</p>
+  </section>
 </template>
