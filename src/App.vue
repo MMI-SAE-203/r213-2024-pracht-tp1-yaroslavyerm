886960b8d9bdf5b8b7d9a71193daedfdfe8dbf9f
yaroslavyerm <yaroslav.yermakov@edu.univ-fcomte.fr>
2024-04-09 15:35:57 +0200

    ajouté @pointerdown

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ const menuIsOpen = ref(false)
       aria-controls="mainNav"
       aria-expanded="true"
       class="rounded-full border-2 border-red-600 bg-red-300 px-2"
+      @pointerdown="menuIsOpen = !menuIsOpen"
       >
       menu
     </button>
